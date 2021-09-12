560df0a37a085927138d9d9775aab469893a0708
Edward Wilson <edward.wil004@gmail.com>
2021-09-12 02:10:41 -0500

    Working on getting GUI working

--- tictactoe.c
+++ tictactoe.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <gtk/gtk.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	
+	GtkWidget *window;
+	GtkWidget *button;
+	gtk_init(&argc, &argv)
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+	gtk_widget_show_all(window);
+	gtk_main();
+
+	return 0;
 }
